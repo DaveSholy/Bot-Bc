787e80f675d3fe84d02295330417bde7381cfe9e
DaveSholy <48659370+DaveSholy@users.noreply.github.com>
2019-04-11 18:11:38 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,5 +16,11 @@ client.on("message", message => {
 });
 
 
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on('ready', () => {
+  client.user.setGame(``,'https://www.twitch.tv/By:A7MD');
+  console.log('BOT ONLINE');
+});
 
 client.login(process.env.BOT_TOKEN);
